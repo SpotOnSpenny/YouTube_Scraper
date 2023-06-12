18a19f85ab8a7fc02c72f77462394609dad40bcd
Spenny <52582291+Appologetic@users.noreply.github.com>
2023-06-12 16:57:08 -0600

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# 0.0.1 (2023-06-10)
+## About this version
+- Search currently ignores all ads and shorts that come up on search
+    - Later could add in a download if there is an ad displayed with search
+    - Later could add in logic for channel/short clicks
+        - For now, elected against it for simplicity for proof of concept
+- Search currently displays all videos that displays on page 1 of results
+    - Later could scroll a random number of times to load more videos and then search
+        - Elected against this for first try, most people will likely click one of the first few results
+        - Later could even weight the randomness so that first few links are more likely to be clicked
+- Search currently randomly selects a video on search
+    - Later could likely add some sort of thumbnail processing to select image that children would be more likely to click on, or weight these videos as more likely to be clicked
+
+## Features
+
---
+++ youtube_scraper/scrape.py
@@ -0,0 +1,48 @@
+# ----- Imports -----
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import random
+
+# ----- Environment Setup -----
+def entrypoint():
+    print("----- Welcome to the M2K YouTube Ad Scraper -----\n")
+    print("This program is designed to scrape a fresh YouTube window for ads based on your defined search term.\n")
+    user_term = input("To begin, please enter the search term you'd like to browse for ads:\n")
+    driver = webdriver.Chrome()
+    get_youtube(driver)
+    search_and_click(driver, user_term)
+    pass
+
+# ----- Starts WebClient and Retreives Youtube -----
+def get_youtube(driver): 
+    driver.get("https://youtube.com")
+    print("Successfully loaded YouTube")
+#TODO add error handling and logging 
+
+
+# ----- Searches for Term and Selects Random Video -----
+def search_and_click(driver, search_term):
+    search_bar = WebDriverWait(driver, timeout=5).until(EC.element_to_be_clickable((By.NAME, "search_query")))
+    print ("Search bar found, conducting search for {}".format(search_term))
+    search_bar.send_keys(search_term)
+    search_bar.send_keys(Keys.RETURN)
+    WebDriverWait(driver, timeout=5).until(EC.title_contains(search_term))
+    search_results = driver.find_elements(By.TAG_NAME, "ytd-video-renderer")
+    number_of_results = len(search_results)
+    random_vid = random.randint(0, number_of_results)
+    print("Found {} videos on page one, clicking on video number {}".format(number_of_results, random_vid))
+    search_results[random_vid].click()
+    pass
+#TODO add error handling and logging
+
+def wait_for_ad():
+    #TODO write script that checks to see if there's an ad
+    pass
+
+def download_ad():
+    #TODO write script that downloads ad once on has been found
+    pass
